c422dbd8ef3a159140bf8eb6f6b044a3b8ec1171
leomatt547 <leo.matt.547@gmail.com>
2021-10-15 12:01:15 +0700

    push final minimax

--- src/ai/minimax.py
+++ src/ai/minimax.py
@@ -22,13 +22,16 @@ class Minimax:
         pass
 
     def create_board_kosong(self):
+        #Membuat board kosong
         board = np.zeros((6, 7))
         return board
 
     def is_valid(self, board, kolom):
+        #Mengecek apakah suatu kolom sudah penuh atau belum
         return board[0, kolom] == 0
 
     def get_kolom_valid(self, board):
+        #Mengembalikan array of kolom yang masih bisa diisi piece
         sel_valid = []
         for kol in range(self.KOLOM_BOARD):
             if self.is_valid(board, kol):
@@ -36,84 +39,76 @@ class Minimax:
         return sel_valid
         
     def taruh_piece(self, boardnya, row, col, piece):
+        #Menaruh piece pada sebuah board (bisa board real atau board jadi-jadian)
         boardnya[row,col] = piece
     
     def get_baris_valid(self, board, kol):
+        #Mendapatkan Baris paling bawah dari suatu kolom yang kosong untuk diisi 
         for bar in range(self.BARIS_BOARD - 1, -1, -1):
             if board[bar,kol] == 0:
                 return bar
 
-    # Mengecek apabila ada langkah kemenangan dengan pion yang sama persis
     def winning_move(self, board, piece):
-        # Check valid horizontal locations for win
+        # Mengecek apabila ada langkah kemenangan dengan piece yang sama persis
+    
+        # Mengecek secara horizontal apakah ada kemenangan atau tidak
         for c in range(self.KOLOM_BOARD-3):
             for r in range(self.BARIS_BOARD):
                 if board[r,c] == piece and board [r,c+1] == piece and board[r,c+2] == piece and board[r,c+3] == piece:
-                    #print("horizontal win")
                     return True
 
-        # Check valid vertical locations for win
+        # Mengecek secara vertikal apakah ada kemenangan atau tidak
         for c in range(self.KOLOM_BOARD):
             for r in range(self.BARIS_BOARD-1, self.BARIS_BOARD-5, -1):
                 if board[r,c] == piece and board[r-1,c] == piece and board[r-2,c] == piece and board[r-3,c] == piece:
-                    #print(piece, "vertical win")
                     return True
 
-        # Check valid positive diagonal locations for win
+        # Mengecek secara diagonal positif apakah ada kemenangan atau tidak
         for c in range(self.KOLOM_BOARD-3):
             for r in range(self.BARIS_BOARD-1, self.BARIS_BOARD-5, -1):
                 if board[r,c] == piece and board [r-1,c+1] == piece and board[r-2,c+2] == piece and board[r-3,c+3] == piece:
-                    #print("diagonal positif win")
                     return True
 
-        # check valid negative diagonal locations for win
+        # Mengecek secara diagonal negatif apakah ada kemenangan atau tidak
         for c in range(self.KOLOM_BOARD-3):
             for r in range(self.BARIS_BOARD-6, self.BARIS_BOARD-4):
                 if board[r,c] == piece and board [r+1,c+1] == piece and board[r+2,c+2] == piece and board[r+3,c+3] == piece:
-                    #print(("diagonal negatif win"))
                     return True
 
-    #Mengecek apabila ada kemenangan yang dikarenakan warna yang sama atau shape yang sama
     def winning_by_element(self, board, piece):
-        #print("piece dalam wbe",piece)
-        if (type(piece) is list):
-            #print("piecenya list:",type(piece) is list)
+        #Mengecek apabila ada kemenangan yang dikarenakan warna piece yang sama atau shape piece yang sama
+
+        if (type(piece) is list): #cek apakah piece berbentuk list?
+            # Mengecek secara horizontal apakah ada kemenangan atau tidak
             for c in range(self.KOLOM_BOARD-3):
                 for r in range(self.BARIS_BOARD):
                     if (board[r,c] in piece) and (board [r,c+1] in piece) and (board[r,c+2] in piece) and (board[r,c+3] in piece):
-                        print("horizontal win",piece)
                         return True
 
-            # Check valid vertical locations for win
+            # Mengecek secara vertikal apakah ada kemenangan atau tidak
             for c in range(self.KOLOM_BOARD):
                 for r in range(self.BARIS_BOARD-1, self.BARIS_BOARD-5, -1):
                     if (board[r,c] in piece) and (board[r-1,c] in piece) and (board[r-2,c] in piece) and (board[r-3,c] in piece):
-                        # print(board[r,c])
-                        # print(board[r-1,c])
-                        # print(board[r-2,c])
-                        # print(board[r-3,c])
-                        print("vertical win",piece)
                         return True
 
-            # Check valid positive diagonal locations for win
+            # Mengecek secara diagonal positif apakah ada kemenangan atau tidak
             for c in range(self.KOLOM_BOARD-3):
                 for r in range(self.BARIS_BOARD-1, self.BARIS_BOARD-5, -1):
                     if (board[r,c] in piece) and (board [r-1,c+1] in piece) and (board[r-2,c+2] in piece) and (board[r-3,c+3] in piece):
-                        print("diagonal positif win", piece)
                         return True
 
-            # check valid negative diagonal locations for win
+            # Mengecek secara diagonal negatif apakah ada kemenangan atau tidak
             for c in range(self.KOLOM_BOARD-3):
                 for r in range(self.BARIS_BOARD-6, self.BARIS_BOARD-4):
                     if (board[r,c] in piece) and (board [r+1,c+1] in piece) and (board[r+2,c+2] in piece) and (board[r+3,c+3] in piece):
-                        print(("diagonal negatif win"),piece)
                         return True
         else:
             return False
 
     def evaluate_window(self, window, piece):
+        # Mengevaluasi setiap window yang dicek, untuk menghitung scorenya
         score = 0
-        if piece is list:
+        if (type(piece) is list): #piece berbentuk list
             # Prioritaskan 3 piece terurut
             if self.hitung_element_beda(window,piece[0], piece[1]) == 3 \
                 and window.count(0) == 1:
@@ -122,7 +117,7 @@ class Minimax:
             elif self.hitung_element_beda(window,piece[0], piece[1]) == 2 \
                 and window.count(0) == 2:
                 score += 2
-        elif piece is int:
+        elif (type(piece) is int): #piece berbentuk int
             # Prioritaskan 3 piece terurut
             if window.count(piece) == 3 and window.count(0) == 1:
                 score += 5
@@ -132,54 +127,54 @@ class Minimax:
         return score
 
     def hitung_element_beda(self, senarai, element1, element2):
+        #Menghitung jumlah piece yang berbeda, tetapi secara lojik sama
         return (senarai.count(element1) + senarai.count(element2))
 
     def score_position(self, board, piece):
+        #Menghitung posisi pada board
         score = 0
 
-        # Score centre column
+        # Utamakan piece dimasukkan pada kolom tengah
         if (type(piece) is int):
-            #print("piecenya integer")
             centre_array = [int(i) for i in list(board[:, self.KOLOM_BOARD // 2])]
             centre_count = centre_array.count(piece)
-            score += centre_count * 8
+            #Apabila ada piece yang akan ditaruh disini, score dikali 8
+            score += centre_count * 3
 
-        # Score horizontal positions
+        # Menilai posisi horizontal
         for r in range(self.BARIS_BOARD-1, -1, -1):
             row_array = [int(i) for i in list(board[r, :])]
             for c in range(self.KOLOM_BOARD - 3):
-                # Create a horizontal window of 4
+                # Membuat window horizontal berukuran 4
                 window = row_array[c:c + 4]
                 score += self.evaluate_window(window, piece)
 
-        # Score vertical positions
+        # Menilai posisi vertikal
         for c in range(self.KOLOM_BOARD):
             col_array = [int(i) for i in list(board[:, c])]
             for r in range(self.BARIS_BOARD-1, self.BARIS_BOARD-4, -1):
-                # Create a vertical window of 4
+                # Membuat window vertikal berukuran 4
                 window = col_array[r:r - 4]
                 score += self.evaluate_window(window, piece)
 
-        # Score positive diagonals
+        # Menilai posisi diagonal positif
         for r in range(self.BARIS_BOARD-1, self.BARIS_BOARD-5, -1):
             for c in range(self.KOLOM_BOARD-3):
-                #Diagonal positif sebesar 4
+                # Membuat window diagonal positif berukuran 4
                 window = [board[r-i, c+i] for i in range(4)]
                 score += self.evaluate_window(window, piece)
         
-
-        # Score negative diagonals
+        # Menilai posisi diagonal negatif
         for r in range(self.BARIS_BOARD-1, self.BARIS_BOARD-4, -1):
             for c in range(self.KOLOM_BOARD-3):
-                # Create a negative diagonal window of 4
+                # Membuat window diagonal negatif berukuran 4
                 window = [board[r - 3 + i, c + i] for i in range(4)]
                 score += self.evaluate_window(window, piece)
         return score
-
-    # Pick the best move by looking at all possible future moves and comparing their scores
+    #Fungsi Utama
     def minimax(self, board, depth, alpha, beta, maximisingPlayer, piece):
         valid_locations = self.get_kolom_valid(board)
-        #print("valid_location",valid_locations)
+        #Fungsi Terminal terjadi apabila ada kondisi yang membuat pertandingan selesai
         is_terminal = self.winning_move(board, 1) \
                         or self.winning_move(board, 2) \
                         or self.winning_move(board, 3) \
@@ -189,69 +184,56 @@ class Minimax:
                         or self.winning_by_element(board, [1,2]) \
                         or self.winning_by_element(board, [3,4]) \
                         or len(valid_locations) == 0
-        #is_terminal = is_terminal_node(board)
         if depth == 0 or is_terminal:
             if is_terminal:
+                #Yang menang kita
                 if ((self.winning_move(board, 1)) \
                     or (self.winning_move(board, 4)) \
                     or (self.winning_by_element(board, [1,4])) \
                     or (self.winning_by_element(board, [1,2]))) :
-                # Weight the bot winning really high
-                    #print("self.winning_move(board, 3) or self.winning_move(board, 2)")
-                    # print("Menang karena winning move 3:",self.winning_move(board, 3))
-                    # print("Menang karena winning move 2:",self.winning_move(board, 2))
-                    # print("Menang karena winning by element [2,3]:", self.winning_by_element(board, [2,3]))
-                    # print("Menang karena winning by element [3,4]:", self.winning_by_element(board, [3,4]))
-                    return (None, 10000000)
-                
-                # Weight the human winning really low
+                    return (None, 10000000) #nilai jadi sangat besar
+                #Yang menang lawan
                 elif ((self.winning_move(board, 3)) \
                     or (self.winning_move(board, 2)) \
                     or (self.winning_by_element(board, [2,3])) \
                     or (self.winning_by_element(board, [3,4]))):
-                    #print("self.winning_move(board, 1) or self.winning_move(board, 4)")
-                    # if(self.winning_by_element(board, [1,4])):
-                    #     print("self.winning_by_element(board, [1,4])", self.winning_by_element(board, [1, 4]))
-                    # if(self.winning_by_element(board, [1,2])):
-                    #     print("self.winning_by_element(board, [1,2])",self.winning_by_element(board, [1,2]))
-                    return (None, -10000000)
-                else: # No more valid moves
-                    #print("No valid moves")
+                    return (None, -10000000) #nilai jadi sangat kecil
+                else:
                     return (None, 0)
-            # Return the bot's score
+            
             else:
+                #Mengembalikan penilaian kondisi board saat ini
+                #Untuk depth teratas, score semakin besar
                 if(piece == self.player.shape):
                     return (None, \
-                        (depth+1)*(self.score_position(board, 1) \
+                        self.score_position(board, 1) \
                         + self.score_position(board, 4)\
                         + self.score_position(board, [1,4]) \
-                        + self.score_position(board, [1,2])))
+                        + self.score_position(board, [1,2]))
                 else:
                     return (None, \
-                        (depth+1)*(self.score_position(board, 3) \
+                        self.score_position(board, 3) \
                         + self.score_position(board, 2)\
                         + self.score_position(board, [2,3]) \
-                        + self.score_position(board, [3,4])))
+                        + self.score_position(board, [3,4]))
 
         if maximisingPlayer:
             value = -math.inf
-            # Randomise column to start
+            #Inisiasi kolomnya dulu supaya tidak null
             column = random.choice(valid_locations)
             for col in valid_locations:
                 row = self.get_baris_valid(board, col)
-                # Create a copy of the board
+                # Membuat copy board
                 b_copy = board.copy()
-                # Drop a piece in the temporary board and record score
+                # Taruh piece di board jadi-jadian dan nilai kondisi sekarang
                 if (piece == self.player.shape):
                     self.taruh_piece(b_copy, row, col, 1)
                 else:
                     self.taruh_piece(b_copy, row, col, 2)
                 new_score = self.minimax(b_copy, depth-1, alpha, beta, False, piece)[1]
-                #print("score di max, kol:", new_score, col)
-                #print(b_copy)
                 if new_score > value:
                     value = new_score
-                    # Make 'column' the best scoring column we can get
+                    # Kolom diganti dengan nilai terbaik
                     column = col
                 alpha = max(alpha, value)
                 if alpha >= beta:
@@ -260,23 +242,21 @@ class Minimax:
 
         else: # Minimising player
             value = math.inf
-            # Randomise column to start
+            #Inisiasi kolomnya dulu supaya tidak null
             column = random.choice(valid_locations)
             for col in valid_locations:
                 row = self.get_baris_valid(board, col)
                 # Create a copy of the board
                 b_copy = board.copy()
-                # Drop a piece in the temporary board and record score
+                # Taruh piece di board jadi-jadian dan nilai kondisi sekarang
                 if (piece == self.player.shape):
                     self.taruh_piece(b_copy, row, col, 3)
                 else:
                     self.taruh_piece(b_copy, row, col, 4)
                 new_score = self.minimax(b_copy, depth-1, alpha, beta, True, piece)[1]
-                #print("score di min, kol:", new_score, col)
-                #print(b_copy)
                 if new_score < value:
                     value = new_score
-                    # Make 'column' the best scoring column we can get
+                    # Kolom diganti dengan nilai terbaik
                     column = col
                 beta = min(beta, value)
                 if alpha >= beta:
@@ -286,10 +266,12 @@ class Minimax:
     def salin_board_dari_state(self, board_asli, n_player):
         player_color = GameConstant.PLAYER_COLOR[n_player]
         if n_player == 0:
+            #Apabila giliran pertama, jadikan piece kita property player 1
             player_shape = GameConstant.PLAYER1_SHAPE
             enemy_color = GameConstant.PLAYER2_COLOR
             enemy_shape = GameConstant.PLAYER2_SHAPE
-        else:
+        else: #n_player = 1
+            #Apabila giliran kedua, jadikan piece kita property player 2
             player_shape = GameConstant.PLAYER2_SHAPE
             enemy_color = GameConstant.PLAYER1_COLOR
             enemy_shape = GameConstant.PLAYER1_SHAPE
@@ -307,31 +289,25 @@ class Minimax:
                     self.board[i, j] = 4
                 else:
                     self.board[i, j] = 0
-        self.piece_player = [1, 4]
-        self.piece_enemy = [3, 2]
         return self.board
 
     def Solusi(self, state, n_player, thinking_time, queue):
+        #Solusi dari permasalahan minimax
         self.board = self.salin_board_dari_state(state.board, n_player)
         self.player = state.players[n_player]
         if(n_player == 1):
             self.enemy = state.players[0]
         else:
             self.enemy = state.players[1]
-        #print(self.board)
+        #Apabila kuota shape player masih cukup
         if(self.player.quota[self.player.shape] > 0):
             kolomnya, scorenya = self.minimax(self.board, 4, -math.inf, math.inf, True, self.player.shape)
-            #print("kolomnya, shape = ",kolomnya,self.player.shape)
             queue.put(kolomnya)
             queue.put(self.player.shape)
-            #return queue
-        else:
-            kolomnya, scorenya = self.minimax(self.board, 4, -math.inf, math.inf, True, self.enemy.shape)
-            #print("kolomnya, shape = ",kolomnya,self.player.shape)
+        else: #kuota shape player sudah habis
+            kolomnya, scorenya = self.minimax(self.board, 4, -math.inf, math.inf, False, self.enemy.shape)
             queue.put(kolomnya)
             queue.put(self.enemy.shape)
-            #return kolomnya, self.enemy.shape
-        #return (random.randint(0, state.self.BARIS_BOARD), random.choice([ShapeConstant.CROSS, ShapeConstant.CIRCLE]))
 
     def find(self, state: State, n_player: int, thinking_time: float) -> Tuple[str, str]:
         self.thinking_time = time() + thinking_time
@@ -341,14 +317,14 @@ class Minimax:
         #Timeout 3 detik
         action_process.join(timeout=3)
 
-        # We send a signal that the other thread should stop.
+        # Kirim signal supaya stop event setelah 3 detik
         action_process.terminate()
         
         #waktu habis, random saja
         if queue.qsize() == 0:
             return (random.randint(0, state.board.col), random.choice([ShapeConstant.CROSS, ShapeConstant.CIRCLE]))
-        #best_movement = self.Solusi(state, n_player, thinking_time) #minimax algorithm
         else:
+            #Queue memiliki isi solusi
             a = queue.get(0)
             b = queue.get(0)
             print(a,b)
